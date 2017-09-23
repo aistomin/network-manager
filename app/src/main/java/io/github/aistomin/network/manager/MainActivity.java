cda3a61cf8259399a8209ec585d9446350b68575
aistomin <andrej.istomin.ikeen@gmail.com>
2017-09-23 15:44:55 +0200

    project skeleton

---
+++ app/src/main/java/io/github/aistomin/network/manager/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.aistomin.network.manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
