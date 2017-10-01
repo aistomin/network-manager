d14f0ed226a6480dd1be70532a7c14b0fb03e91c
aistomin <andrej.istomin.ikeen@gmail.com>
2017-10-01 20:59:47 +0200

    MainActivity.java: unused import was deleted.

--- app/src/main/java/io/github/aistomin/network/manager/MainActivity.java
+++ app/src/main/java/io/github/aistomin/network/manager/MainActivity.java
@@ -10,7 +10,6 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
